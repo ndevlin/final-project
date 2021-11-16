d78287662069befac6a7b56aadfc9aae372ebbb1
Nathan Devlin <ndevlin@seas.upenn.edu>
2021-11-16 17:25:48 -0500

    test

--- src/shaders/flat-frag.glsl
+++ src/shaders/flat-frag.glsl
@@ -9,5 +9,5 @@ in vec2 fs_Pos;
 out vec4 out_Col;
 
 void main() {
-  out_Col = vec4(0.5 * (fs_Pos + vec2(1.0)), 0.5 * (sin(u_Time * 3.14159 * 0.01) + 1.0), 1.0);
+  out_Col = vec4(1.0 * (fs_Pos + vec2(1.0)), 0.5 * (sin(u_Time * 3.14159 * 0.01) + 1.0), 1.0);
 }
